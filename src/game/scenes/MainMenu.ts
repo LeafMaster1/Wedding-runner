1902da69a19db18dd1c0ad61b4f4be77f84eb501
Marcus Löf <LeafMaster1@outlook.com>
2026-05-11 12:54:19 +0200

    event bus gör så att phaser och svelte håller koll på ljudet när det ändras i settings

--- src/game/scenes/MainMenu.ts
+++ src/game/scenes/MainMenu.ts
@@ -1,6 +1,7 @@
 import { Scene } from 'phaser';
 import { EventBus } from '../EventBus';
-
+import { audioSettings } from '../../lib/audioStore';
+import { get } from 'svelte/store';
 export class MainMenu extends Scene
 {
     constructor ()
@@ -15,10 +16,21 @@ export class MainMenu extends Scene
         if (!music.isPlaying) {
             music.play({ volume: 0.2 });
         }
+
+        const settings = get(audioSettings);
+        this.sound.volume = settings.volume;
+        this.sound.mute = settings.isMuted;
         
         // Background image
         const background = this.add.image(512, 384, 'home').setDepth(0);
         background.setDisplaySize(1024, 768);
+
+        EventBus.on('change-volume', (newVolume: number) => {
+            this.sound.setVolume(newVolume);
+        });
+        EventBus.on('change-muted', (isMuted: boolean) => {
+            this.sound.setMute(isMuted);
+        });
 
         // Tell Svelte that the scene is ready
         EventBus.emit('current-scene-ready', this);
